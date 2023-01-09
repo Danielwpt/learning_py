9abda432642bdf41f1d049d3d70c88c6e0d69384
Daniel <danielWong151999@gmail.com>
2023-01-09 14:54:14 +0800

    Learn Python init and str functions v2

--- basics/main.py
+++ basics/main.py
@@ -3,6 +3,9 @@ class Person:
         self.name = name
         self.age = age
 
+    def __str__(self):
+        return f"His name is {self.name} Age is {self.age}"
+
 Adam = Person("Adam", 12)
 
-print(Adam.name + " " + str(Adam.age))
+print(Adam)
