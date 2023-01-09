781c7219c29d375ea98982738f9b5fc194c61189
Daniel <danielWong151999@gmail.com>
2023-01-09 16:39:34 +0800

    Add a child class of Person

--- basics/main.py
+++ basics/main.py
@@ -1,11 +1,17 @@
 class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+    def __init__(own, name, age):
+        own.name = name
+        own.age = age
 
-    def __str__(self):
-        return f"His name is {self.name} Age is {self.age}"
+    def __str__(own):
+        return f"His name is {own.name} Age is {own.age}"
+
+class Police(Person):
+    pass
 
 Adam = Person("Adam", 12)
 
+Ali = Police("Ali", 34)
+
 print(Adam)
+print(Ali.__str__())
